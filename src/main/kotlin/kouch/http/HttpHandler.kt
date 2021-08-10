55d8c7b508740dfb6b250ffa5a93c726e1da1466
CmdrNorthpaw <thetuxkid@gmail.com>
2021-08-10 21:57:12 +0100

    Publicise RequestBuilder typealias and add payloads to put and post

--- src/main/kotlin/kouch/http/HttpHandler.kt
+++ src/main/kotlin/kouch/http/HttpHandler.kt
@@ -3,19 +3,20 @@ package kouch.http
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import kouch.http.auth.AuthToken
 import org.json.JSONObject
 
 interface HttpHandler {
 
     suspend fun get(route: String, builder: RequestBuilder = {}): HttpResponse
 
-    suspend fun post(route: String, builder: RequestBuilder = {}): HttpResponse
+    suspend fun post(route: String, payload: JSONObject, builder: RequestBuilder = {}): HttpResponse
 
     suspend fun delete(route: String, builder: RequestBuilder = {}): HttpResponse
 
-    suspend fun put(route: String, builder: RequestBuilder = {}): HttpResponse
+    suspend fun put(route: String, payload: JSONObject, builder: RequestBuilder = {}): HttpResponse
 
     suspend fun authenticate(username: String, password: String): AuthToken
 }
 
-private typealias RequestBuilder = HttpRequestBuilder.() -> Unit
+typealias RequestBuilder = HttpRequestBuilder.() -> Unit
