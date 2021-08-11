911b3192e8a5915d1ca796494320d6c01c6de2c8
CmdrNorthpaw <thetuxkid@gmail.com>
2021-08-11 12:03:26 +0100

    Rewrite the CouchDatabase class so that it is not broken

--- src/main/kotlin/kouch/CouchDatabase.kt
+++ src/main/kotlin/kouch/CouchDatabase.kt
@@ -3,43 +3,33 @@ package kouch
 import io.ktor.client.*
 import io.ktor.client.engine.java.*
 import io.ktor.http.*
-import kouch.http.AuthToken
-import kouch.http.AuthenticationException
-import kouch.http.HttpHandler
-import kouch.http.HttpResponse
+import kouch.annotations.KouchDsl
+import kouch.http.*
 import org.json.JSONObject
 
-class CouchDatabase(
-    override val databaseId: String,
-    override val host: String,
-    override val port: Int = 5294,
-    override val useHttps: Boolean = true,
-    useHttp2: Boolean = true,
-) : HttpHandler {
-    override val client = HttpClient(Java) {
-        followRedirects = true
-        engine {
-            config {
-                version(if (useHttp2) java.net.http.HttpClient.Version.HTTP_2 else java.net.http.HttpClient.Version.HTTP_1_1)
+open class CouchDatabase(
+    val dbName: String,
+    val connector: CouchDbConnector,
+    username: String,
+    password: String
+) : HttpHandler by connector {
+    @KouchDsl
+    class Builder {
+        var dbName: String? = null
+        var username: String? = null
+        var password: String? = null
+        private lateinit var connector: CouchDbConnector
 
-            }
+        fun connector(builder: CouchDbConnector.Builder.() -> Unit) {
+            connector = CouchDbConnector.Builder().apply(builder).build()
         }
-    }
 
-    override lateinit var token: String
+        fun build(): CouchDatabase {
+            val name = requireNotNull(this.dbName)
+            val username = requireNotNull(this.username)
+            val password = requireNotNull(this.password)
 
-
-    suspend fun authenticate(username: String, password: String): AuthToken {
-        val loginResponse = post("/_session") {
-            body = JSONObject()
-                .append("name", username)
-                .append("password", password)
-        }.responseData
-        if (loginResponse.statusCode == HttpStatusCode.Unauthorized) {
-            throw AuthenticationException("Invalid username or password!")
-        } else {
-            val token = loginResponse.headers["Set-Cookie"]!!.split("; ")[0].split("=")[1]
-            return AuthToken(token)
+            return CouchDatabase(name, connector, username, password)
         }
     }
 }
